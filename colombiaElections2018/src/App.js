03905f0c37cf254edd9da0abcc04ae4d34880cc8
daviddlaiton <ad.laiton10@uniandes.edu.co>
2018-07-04 00:22:25 -0500

    React added

---
+++ colombiaElections2018/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid"> 
+        <header className="App-header">
+          <br/>
+          <h1 className="App-title">Elecciones Colombia 2018</h1>
+        </header>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
